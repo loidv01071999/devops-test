2e80ecdf1060c9479cc80c87354c38461664df6d
Loi DV <loidv01071999@gmail.com>
2024-02-22 18:43:29 +0700

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const pool = new Pool({
 // Create users table if not exists
 pool.query(`
   CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
